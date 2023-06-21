b0e989e8eabc52c86d10f35afaf98424c1ebb9c7
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-21 11:40:09 +0400

    display scroll in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
+import InfiniteScrollComponent from './components/InfiniteScrollComponent/InfiniteScrollComponent'
 
 function App() {
 
   return (
-    <>
-    </>
+    <InfiniteScrollComponent />
   )
 }
 
