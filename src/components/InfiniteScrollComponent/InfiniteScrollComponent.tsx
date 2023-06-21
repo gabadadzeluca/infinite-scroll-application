29d334f0a246d54a023804e2c605401f4a249160
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-21 11:29:23 +0400

    add postComponent and correct types; add basic error chacking

--- src/components/InfiniteScrollComponent/InfiniteScrollComponent.tsx
+++ src/components/InfiniteScrollComponent/InfiniteScrollComponent.tsx
@@ -1,52 +1,66 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import { DataObjectInterface } from "../../utils/DataObjectInterface";
+import PostComponent from "../PostComponent/PostComponent";
+import styles from "./InfiniteScrollComponent.module.css";
 
-const ITEMS_PER_PAGE = 50;
+const ITEMS_COUNT = 5000;
+const ITEMS_PER_PAGE = 20;
+const LAST_PAGE_INDEX = Math.floor(ITEMS_COUNT/ITEMS_PER_PAGE) - 1;
 
 export default function InfiniteScrollComponent() {
   const [data, setData] = useState<DataObjectInterface[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
-  const startIndex = currentPage * ITEMS_PER_PAGE;
-  const endIndex = (currentPage + 1) * ITEMS_PER_PAGE;
-    const observer = useRef<IntersectionObserver | null>(null);
+  const startIndex = (currentPage-1) * ITEMS_PER_PAGE;
+  const endIndex = (currentPage) * ITEMS_PER_PAGE;
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  const lastItemRef = useCallback((node: HTMLDivElement)=>{
-    if(!isLoaded) return;
-    if(observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver((entries:IntersectionObserverEntry[])=>{
-      if(entries[0].isIntersecting) setCurrentPage((prevPage)=>prevPage + 1);
-    });
-    if(node) observer.current.observe(node);
-  },[isLoaded]);
+  const lastItemRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (!isLoaded) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(
+        (entries: IntersectionObserverEntry[]) => {
+          if (entries[0].isIntersecting)
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+      );
+      if (node) observer.current.observe(node);
+    },
+    [isLoaded]
+  );
 
-  const fetchData = async () =>{
-    console.log("MAKING REQUEST")
+  const fetchData = async () => {
     const API_URL = `https://jsonplaceholder.typicode.com/photos/?_start=${startIndex}&_limit=${endIndex}`;
-    try{
+    console.log('Making Request')
+    if (currentPage > LAST_PAGE_INDEX) {
+      console.log("LIMIT REACHED");
+      return;
+    }
+    try {
       const res = await axios.get(API_URL);
       setData(res.data);
       setIsLoaded(true);
-    }catch(error){
+    } catch (error) {
+      setIsLoaded(false);
       console.log(error); // test
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     setIsLoaded(false);
     fetchData();
-  },[currentPage])
-  console.log(currentPage);
+  }, [currentPage]);
+
   return (
-    <div>
+    <div className={styles.container}>
       {data.map((post, index) => {
-        if(index + 1 === data.length){
-          // fetchData();
-          return <div key={index} ref={lastItemRef}>{post.id}</div>
+        if (index + 1 === data.length) {
+          return <PostComponent key={index} ref={lastItemRef} post={post} />;
         }
-        return <div key={index}>{post.id}</div>;
+        return <PostComponent key={index} post={post} />;
       })}
       {!isLoaded && <div>Loading...</div>}
     </div>
